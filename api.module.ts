e76d8c46eb724b452a9347f555adde3758bc9790
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2020-07-16 11:13:36 +0300

    add current sources

---
+++ api.module.ts
@@ -0,0 +1,51 @@
+import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
+import { Configuration } from './configuration';
+import { HttpClient } from '@angular/common/http';
+
+
+import { CommandService } from './api/command.service';
+import { DataService } from './api/data.service';
+import { GatewayService } from './api/gateway.service';
+import { GroupService } from './api/group.service';
+import { ParticipatorySensingService } from './api/participatorySensing.service';
+import { PhenomenonService } from './api/phenomenon.service';
+import { RegistryService } from './api/registry.service';
+import { ResourceService } from './api/resource.service';
+import { UnitService } from './api/unit.service';
+import { UnitConversionService } from './api/unitConversion.service';
+
+@NgModule({
+  imports:      [],
+  declarations: [],
+  exports:      [],
+  providers: [
+    CommandService,
+    DataService,
+    GatewayService,
+    GroupService,
+    ParticipatorySensingService,
+    PhenomenonService,
+    RegistryService,
+    ResourceService,
+    UnitService,
+    UnitConversionService ]
+})
+export class ApiModule {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+        return {
+            ngModule: ApiModule,
+            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
+        };
+    }
+
+    constructor( @Optional() @SkipSelf() parentModule: ApiModule,
+                 @Optional() http: HttpClient) {
+        if (parentModule) {
+            throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+        }
+        if (!http) {
+            throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+            'See also https://github.com/angular/angular/issues/20575');
+        }
+    }
+}
---
+++ api/api.js
@@ -0,0 +1,26 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./command.service"));
+var command_service_1 = require("./command.service");
+__export(require("./data.service"));
+var data_service_1 = require("./data.service");
+__export(require("./gateway.service"));
+var gateway_service_1 = require("./gateway.service");
+__export(require("./group.service"));
+var group_service_1 = require("./group.service");
+__export(require("./participatorySensing.service"));
+var participatorySensing_service_1 = require("./participatorySensing.service");
+__export(require("./phenomenon.service"));
+var phenomenon_service_1 = require("./phenomenon.service");
+__export(require("./registry.service"));
+var registry_service_1 = require("./registry.service");
+__export(require("./resource.service"));
+var resource_service_1 = require("./resource.service");
+__export(require("./unit.service"));
+var unit_service_1 = require("./unit.service");
+__export(require("./unitConversion.service"));
+var unitConversion_service_1 = require("./unitConversion.service");
+exports.APIS = [command_service_1.CommandService, data_service_1.DataService, gateway_service_1.GatewayService, group_service_1.GroupService, participatorySensing_service_1.ParticipatorySensingService, phenomenon_service_1.PhenomenonService, registry_service_1.RegistryService, resource_service_1.ResourceService, unit_service_1.UnitService, unitConversion_service_1.UnitConversionService];
