5d26a5a0a0ef145e5e5a5e2515922915a52b929d
kliv <kostas.livs@gmail.com>
2020-10-23 13:36:23 +0300

    fix ModuleWithProviders error

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
+import {Provider, Type} from "@angular/core";
+
 export * from './api/api';
 export * from './model/models';
 export * from './variables';
 export * from './configuration';
 export * from './api.module';
+
+declare module "@angular/core" {
+    interface ModuleWithProviders<T = any> {
+        ngModule: Type<T>;
+        providers?: Provider[];
+    }
+}
